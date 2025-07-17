52e8dfa31fcef0e5fd39173b0b90b0641896b8cf
Phongsakorn Tangpok <664259011@webmail.npru.ac.th>
2025-07-17 13:28:21 +0700

    api crud

--- server/routers/restaurant.router.js
+++ server/routers/restaurant.router.js
@@ -5,5 +5,8 @@ const router = express.Router();
 
 // POST http://localhost:5000/api/v1/restaurants
 router.post("/", restaurantController.create);
-
+router.get('/', restaurantController.getAll);
+router.get('/:id', restaurantController.getById);
+router.put('/:id', restaurantController.update);
+router.delete('/:id', restaurantController.deleteById);
 module.exports = router;
